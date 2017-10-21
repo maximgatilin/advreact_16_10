107ad6901c39cae26c13f5d6a8587598eb7cfc4e
Gatilin Maxim <gatilin222@gmail.com>
2017-10-21 16:23:29 +0300

    #HT1 Add people form

---
+++ src/components/people/Add.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import {reduxForm, Field} from 'redux-form'
+import emailValidator from 'email-validator'
+import ErrorField from '../common/ErrorField'
+
+class PeopleAdd extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Add people</h3>
+                <form onSubmit={this.props.handleSubmit}>
+                    <Field name='firstName' component={ErrorField} type='text' label='First Name'/>
+                    <Field name='lastName' component={ErrorField} type='text' label='Last Name'/>
+                    <Field name='email' component={ErrorField} type='text' label='Email'/>
+                    <div>
+                        <input type='submit' disabled={this.props.invalid}/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+const validate = ({firstName, lastName, email}) => {
+    const errors = {}
+
+    if (!email) errors.email = 'email is a required field'
+    if (!firstName) errors.firstName = 'First name is a required field'
+    if (!lastName) errors.lastName = 'Last name is a required field'
+
+    if (email && !emailValidator.validate(email)) errors.email = 'invalid email'
+
+    return errors
+}
+
+export default reduxForm({
+    form: 'people_add',
+    validate
+})(PeopleAdd)
--- src/components/routes/People.js
+++ src/components/routes/People.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import {Route, NavLink} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {addPeopleEntry} from '../../ducks/people'
+import AddPeople from '../people/Add';
 
 class PeoplePage extends Component {
     render() {
         return (
             <div>
-                <h2>People</h2>
+              <h2>People</h2>
+              <NavLink to = '/people/add' activeStyle = {{color: 'red'}}>add</NavLink>
+              <Route path = '/people/add' render = {() => <AddPeople onSubmit = {this.handleAdd}/>}/>
             </div>
         )
     }
+
+    handleAdd = (entry) => this.props.addPeopleEntry(entry)
 }
 
-export default PeoplePage;
+export default connect(null, { addPeopleEntry })(PeoplePage)
